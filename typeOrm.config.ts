6a0a6049217130fad30fa8668b36852eef953af8
allextone <visloguzov.aleks@gmail.com>
2024-08-16 15:55:44 +0300

    refactor

--- typeOrm.config.ts
+++ typeOrm.config.ts
@@ -1,17 +1,25 @@
+import { ConfigService } from '@nestjs/config';
 import { DataSource } from 'typeorm';
 import { config } from 'dotenv';
 import { entities } from './src/infrastructure/db/postgres/entites';
 import { migrations } from './migrations';
+import { MyCustomLogger } from './src/infrastructure/logger/typeorm-logger.service';
 
 config();
 
-export default new DataSource({
+const configService: ConfigService = new ConfigService();
+
+export const AppDataSource = new DataSource({
   type: 'postgres',
-  host: process.env.POSTGRES_HOST,
-  port: parseInt(<string>process.env.POSTGRES_PORT),
-  username: process.env.POSTGRES_USER,
-  password: process.env.POSTGRES_PASSWORD,
-  database: process.env.POSTGRES_DB,
+  host: configService.get('POSTGRES_HOST'),
+  port: parseInt(<string>configService.get('POSTGRES_PORT')),
+  username: configService.get('POSTGRES_USER'),
+  password: configService.get('POSTGRES_PASSWORD'),
+  database: configService.get('POSTGRES_DB'),
   entities,
   migrations,
+  synchronize: true,
+  logNotifications: true,
+  logging: 'all',
+  logger: new MyCustomLogger(),
 });
